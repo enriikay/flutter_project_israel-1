fbd3352e9afbcf3d55d5a86cdf5a18c04a82ae12
github <djisrael05@gmail.com>
2025-10-28 12:45:13 +0800

    Hotfix
    - Added newsfeed_post.dart
    - Minor changes to newsfeed.dart
    - Major changes to storieslist.dart

--- secondflutter/lib/pages/newsfeed.dart
+++ secondflutter/lib/pages/newsfeed.dart
@@ -1,4 +1,7 @@
+import 'package:secondflutter/model/story_model.dart';
 import 'package:secondflutter/widget/createpost.dart';
+import 'package:secondflutter/widget/newsfeed_post.dart';
+import 'package:secondflutter/widget/storieslist.dart';
 import 'package:flutter/material.dart';
 
 class Newsfeed extends StatefulWidget {
@@ -9,6 +12,53 @@ class Newsfeed extends StatefulWidget {
 }
 
 class _NewsfeedState extends State<Newsfeed> {
+  List<StoryModel> stories = [
+    StoryModel(
+      id: "1",
+      username: "Juan",
+      profileImagePath: "assets/profile/prof1.jpg",
+      storyImagePath: "assets/myday/myday1.jpg",
+      timestamp: DateTime.now().subtract(const Duration(hours: 5)),
+      isViewed: false,
+    ),
+
+    StoryModel(
+      id: "2",
+      username: "Clara",
+      profileImagePath: "assets/profile/prof2.jpg",
+      storyImagePath: "assets/myday/myday2.jpg",
+      timestamp: DateTime.now().subtract(const Duration(hours: 5)),
+      isViewed: false,
+    ),
+
+    StoryModel(
+      id: "3",
+      username: "Maria",
+      profileImagePath: "assets/profile/prof3.jpg",
+      storyImagePath: "assets/myday/myday3.jpg",
+      timestamp: DateTime.now().subtract(const Duration(hours: 5)),
+      isViewed: false,
+    ),
+
+    StoryModel(
+      id: "4",
+      username: "Roberto",
+      profileImagePath: "assets/profile/prof4.jpg",
+      storyImagePath: "assets/myday/myday4.jpg",
+      timestamp: DateTime.now().subtract(const Duration(hours: 5)),
+      isViewed: false,
+    ),
+
+    StoryModel(
+      id: "5",
+      username: "Alfonso",
+      profileImagePath: "assets/profile/prof5.jpg",
+      storyImagePath: "assets/myday/myday5.jpg",
+      timestamp: DateTime.now().subtract(const Duration(hours: 5)),
+      isViewed: false,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,7 +70,29 @@ class _NewsfeedState extends State<Newsfeed> {
           DrawerButton(),
         ],
       ),
-      body: Column(children: [Createpost(onpostcreate: () {})]),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Createpost(),
+            StoriesList(stories: stories, onStoryTap: (story) {}),
+            NewsFeedPost(
+              name: "John Doe",
+              caption: "This is a sample caption for John's post.",
+              imageurl: "assets/myday/myday1.jpg",
+            ),
+            NewsFeedPost(
+              name: "Jane Doe",
+              caption: "This is a sample caption for John's post.",
+              imageurl: "assets/myday/myday1.jpg",
+            ),
+            NewsFeedPost(
+              name: "Juan Perez",
+              caption: "This is a sample caption for John's post.",
+              imageurl: "assets/myday/myday1.jpg",
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
